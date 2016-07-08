4aabdceb48b182c749d3d0c22028474a9cabc502
kurage4410 <watanabe@diverta.co.jp>
2016-07-08 14:24:00 +0900

    fix the order of initialize (to stop some warning)

--- unit/TailController.cpp
+++ unit/TailController.cpp
@@ -1,14 +1,14 @@
 #include "TailController.h"
+#include <math.h>
 #define GAIN 2.5F
 
 
 TailController::TailController(ev3api::Motor& tailMotor)
   :mTailMotor(tailMotor),
-  mForward(0),
-  mAngle(tailMotor.getCount()){}
+  mAngle(3){}
 
 void TailController::run(){
-  int speed = calcDirection();
+  float speed = calcDirection();
   //int speed=-1;
   mTailMotor.setPWM(speed);
 }
@@ -17,8 +17,10 @@ void TailController::init(){
   mTailMotor.reset();
 }
 int TailController::calcDirection(){
-  int tailEnc  = mTailMotor.getCount();        // 左モータ回転角度
-  return (tailEnc-mAngle)*0.25;
+  int diff=mTailMotor.getCount()-mAngle;
+  if(diff==0)return 0;
+  else if(diff<0) return -1;
+  else return 1;
 }
 void TailController::setAngle(int angle){
   mAngle=angle;
